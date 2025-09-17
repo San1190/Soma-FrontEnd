e8cd547b70253e6a11f7636ae1e0983a755ccc44
San1190 <sanfuenal@gmail.com>
2025-09-17 14:03:09 +0200

    contador prueba

--- Soma/App.js
+++ Soma/App.js
@@ -1,10 +1,25 @@
+// App.js
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+// 1. ¡IMPORTAMOS NUESTRO JUGUETE!
+// Le decimos a App.js dónde encontrar el componente Contador.
+import Contador from './src/components/Contador';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>¡Bienvenido a mi App de Juguetes!</Text>
+      
+      {/* 2. ¡USAMOS NUESTRO JUGUETE AQUÍ!
+          Lo llamamos como si fuera una etiqueta de HTML. */}
+      <Contador />
+
+      {/* Puedes añadir más si quieres, ¡son reutilizables! */}
+      <Contador />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +32,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  }
 });

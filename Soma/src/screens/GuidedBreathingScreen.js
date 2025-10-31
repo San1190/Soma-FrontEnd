9129dffab7374a4aa4854ca353ae2e6beae5c139
Serxu9 <scuecer@upv.edu.es>
2025-10-31 20:21:42 +0100

    correcciones

--- Soma/src/screens/GuidedBreathingScreen.js
+++ Soma/src/screens/GuidedBreathingScreen.js
@@ -12,7 +12,7 @@ const GuidedBreathingScreen = ({ navigation }) => {
       const fetchStressLevel = async () => {
         if (user && user.userId) {
           try {
-            const response = await fetch(`http://192.168.1.141:8080/api/stress/users/${user.userId}`);
+            const response = await fetch(`http://192.168.1.31:8080/api/stress/users/${user.userId}`);
             const data = await response.json();
             setStressLevel(data.stressLevel); // Asume que el backend devuelve { stressLevel: "Bajo" }
           } catch (error) {
